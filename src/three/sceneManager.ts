5d53a6041c5c68eec107aab32c56baff6e9b1c8f
伊钱耀 <yiqianyao@bytedance.com>
2025-01-24 17:02:37 +0800

    feat: add first particle demo

--- src/three/sceneManager.ts
+++ src/three/sceneManager.ts
@@ -1,18 +1,24 @@
+// @ts-nocheck
 import * as THREE from 'three';
+import { Debug, Proton } from './particle';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 export class SceneManager {
     renderer: THREE.WebGLRenderer;
+    proton!: Proton
     constructor(wrap: HTMLDivElement) {
          // 创建场景
          const scene = new THREE.Scene();
 
          // 创建相机
-         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-         camera.position.z = 5;
+         const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+         camera.position.z = 500;
+         const control = new OrbitControls(camera, wrap);
+         control.update();
  
          // 创建渲染器
          const renderer = new THREE.WebGLRenderer({
              antialias: true,
-             // alpha: true,
+             alpha: true,
          });
          this.renderer = renderer;
          renderer.setSize(window.innerWidth, window.innerHeight);
@@ -20,18 +26,89 @@ export class SceneManager {
  
          // 创建几何体
          const geometry = new THREE.BoxGeometry();
-         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-         const cube = new THREE.Mesh(geometry, material);
-         scene.add(cube);
+         const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+         const mesh = new THREE.Mesh(geometry, material);
+         scene.add(mesh);
+
+         var ambientLight = new THREE.AmbientLight();
+        scene.add(ambientLight);
+
+        var pointLight = new THREE.PointLight(0xffffff, 2, 1000, 1);
+        pointLight.position.set(0, 1000, 0);
+        scene.add(pointLight);
+
+         const proton = this.initProton(scene, mesh);
  
          // 渲染循环
          const animate = () => {
-             requestAnimationFrame(animate);
-             cube.rotation.x += 0.01;
-             cube.rotation.y += 0.01;
-             renderer.render(scene, camera);
+            control.update();
+            proton.update();
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
          };
  
          animate();
+    }
+
+    initProton(scene, mesh) {
+        const proton = new Proton();
+        proton.addEmitter(this.createEmitter(scene, proton));
+
+        //add custom renderer
+        var renderer = new Proton.CustomRender();
+        renderer.onParticleCreated = function(p) {
+            //p.target = mesh.clone();
+            p.target = this.targetPool.get(mesh);
+            p.target.position.copy(p.p);
+            scene.add(p.target);
+        }
+
+        renderer.onParticleUpdate = function(p) {
+            p.target.position.copy(p.p);
+            p.target.rotation.set(p.rotation.x, p.rotation.y, p.rotation.z);
+
+            var scale = p.scale * 30;
+            p.target.scale.set(scale, scale, scale);
+        }
+
+        renderer.onParticleDead = function(p) {
+            this.targetPool.expire(p.target);
+            scene.remove(p.target);
+            p.target = null;
+        }
+
+        proton.addRender(renderer);
+        return proton;
+    }
+
+    createEmitter(scene: THREE.Scene, proton: Proton) {
+        const emitter = new Proton.Emitter();
+        
+        emitter.rate = new Proton.Rate(new Proton.Span(4, 8), new Proton.Span(.2, .5));
+        emitter.addInitialize(new Proton.Mass(1));
+        emitter.addInitialize(new Proton.Radius(100));
+        emitter.addInitialize(new Proton.Life(2, 4));
+        emitter.addInitialize(new Proton.Velocity(400, new Proton.Vector3D(0, 1, 0), 60));
+
+        // //emitter.addBehaviour(new Proton.RandomDrift(30, 30, 30, .05));
+        emitter.addBehaviour(new Proton.Rotate("random", "random"));
+        emitter.addBehaviour(new Proton.Scale(1, .1));
+        emitter.addBehaviour(new Proton.G(6));
+
+        var zone = new Proton.BoxZone(600);
+        zone.friction = 0.95;
+        zone.max = 7;
+        emitter.addBehaviour(new Proton.CrossZone(zone, "bound"));
+        emitter.addBehaviour(new Proton.Color(0xff0000, 'random', Infinity, Proton.easeOutQuart));
+
+        // @ts-ignore
+        emitter.p.x = 0;
+        // @ts-ignore
+        emitter.p.y = 0;
+        
+        emitter.emit();
+        Debug.drawZone(proton, scene, zone);
+
+        return emitter;
     }
 }
