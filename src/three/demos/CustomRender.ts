dcd43e0cc86259d6b4aba77d2a678463ec29edd8
伊钱耀 <yiqianyao@bytedance.com>
2025-01-29 20:39:18 +0800

    feat: update code

---
+++ src/three/demos/CustomRender.ts
@@ -0,0 +1,99 @@
+// @ts-ignore
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Camera, Mesh, Scene, WebGLRenderer } from "three";
+import { Proton } from "../particle/core";
+import { CustomRender } from "../particle/render/CustomRender";
+import { Emitter } from "../particle/emitter/Emitter";
+import { Rate } from "../particle/initialize/Rate";
+import { Span } from "../particle/math/Span";
+import { Rotate } from "../particle/Behaviour/Rotate";
+import { Scale } from "../particle/Behaviour/Scale";
+import { BoxZone } from "../particle/zone/BoxZone";
+import { Gravity } from "../particle/Behaviour/Gravity";
+import { CrossZone } from "../particle/Behaviour/CrossZone";
+import { Color } from "../particle/Behaviour/Color";
+import { ease } from "../particle/ease/ease";
+import { Debug } from "../particle/debug/debug";
+import { Mass } from '../particle/initialize/Mass';
+import { Radius } from '../particle/initialize/Radius';
+import { Life } from '../particle/initialize/Life';
+import { Velocity } from '../particle/initialize/Velocity';
+import { Vector3D } from '../particle/math/Vector3D.js';
+
+export function initCustomRenderParticle(scene: Scene, camera: Camera, renderer: WebGLRenderer, control: OrbitControls, mesh: Mesh) {
+    const proton = initProton(scene, mesh);
+    // 渲染循环
+    const animate = () => {
+        control.update();
+        proton.update();
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    };
+
+    animate();
+}
+
+function initProton(scene: Scene, mesh: Mesh) {
+    const proton = new Proton();
+    proton.addEmitter(createEmitter(scene, proton));
+
+    //add custom renderer
+    var renderer = new CustomRender();
+    renderer.onParticleCreated = function(p) {
+        //p.target = mesh.clone();
+        p.target = this.targetPool.get(mesh);
+        p.target.position.copy(p.p);
+        scene.add(p.target);
+    }
+
+    renderer.onParticleUpdate = function(p) {
+        p.target.position.copy(p.p);
+        p.target.rotation.set(p.rotation.x, p.rotation.y, p.rotation.z);
+
+        var scale = p.scale * 30;
+        p.target.scale.set(scale, scale, scale);
+    }
+
+    renderer.onParticleDead = function(p) {
+        this.targetPool.expire(p.target);
+        scene.remove(p.target);
+        p.target = null;
+    }
+
+    proton.addRender(renderer);
+    return proton;
+}
+
+function createEmitter(scene: Scene, proton: Proton) {
+    const emitter = new Emitter();
+    
+    emitter.rate = new Rate(new Span(4, 8), new Span(.2, .5));
+    // @ts-ignore
+    emitter.addInitialize(new Mass(1));
+    // @ts-ignore
+    emitter.addInitialize(new Radius(100));
+    // @ts-ignore
+    emitter.addInitialize(new Life(2, 4));
+    // @ts-ignore
+    emitter.addInitialize(new Velocity(400, new Vector3D(0, 1, 0), 60));
+
+    emitter.addBehaviour(new Rotate("random", "random"));
+    emitter.addBehaviour(new Scale(1, .1));
+    emitter.addBehaviour(new Gravity(6));
+
+    var zone = new BoxZone(600);
+    zone.friction = 0.95;
+    zone.max = 7;
+    emitter.addBehaviour(new CrossZone(zone, "bound"));
+    emitter.addBehaviour(new Color(0xff0000, 'random', Infinity, ease.easeOutQuart));
+
+    // @ts-ignore
+    emitter.p.x = 0;
+    // @ts-ignore
+    emitter.p.y = 0;
+    // @ts-ignore
+    emitter.emit();
+    Debug.drawZone(proton, scene, zone);
+
+    return emitter;
+}
--- src/three/sceneManager.ts
+++ src/three/sceneManager.ts
@@ -2,23 +2,7 @@
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import * as THREE from 'three';
 import { Proton } from './particle/core';
-import { Span } from './particle/math/Span.js';
-import { CustomRender } from './particle/render/CustomRender';
-import { Debug } from './particle/debug/debug.js';
-import { Vector3D } from './particle/math/Vector3D.js';
-import { BoxZone } from './particle/zone/BoxZone.js';
-import { ease } from './particle/ease/ease.js';
-import { Gravity } from './particle/Behaviour/Gravity.js';
-import { CrossZone } from './particle/Behaviour/CrossZone.js';
-import { Scale } from './particle/Behaviour/Scale';
-import { Rotate } from './particle/Behaviour/Rotate';
-import { Color } from './particle/Behaviour/Color';
-import { Rate } from './particle/initialize/Rate';
-import { Life } from './particle/initialize/Life';
-import { Velocity } from './particle/initialize/Velocity';
-import { Mass } from './particle/initialize/Mass';
-import { Radius } from './particle/initialize/Radius';
-import { Emitter } from './particle/emitter/Emitter';
+import { initCustomRenderParticle } from "./demos/CustomRender";
 
 // https://github.com/drawcall/three.proton
 export class SceneManager {
@@ -56,81 +40,10 @@ export class SceneManager {
         pointLight.position.set(0, 1000, 0);
         scene.add(pointLight);
 
-         const proton = this.initProton(scene, mesh);
- 
-         // 渲染循环
-         const animate = () => {
-            control.update();
-            proton.update();
-            requestAnimationFrame(animate);
-            renderer.render(scene, camera);
-         };
- 
-         animate();
-    }
-
-    initProton(scene: THREE.Scene, mesh: THREE.Mesh) {
-        const proton = new Proton();
-        proton.addEmitter(this.createEmitter(scene, proton));
-
-        //add custom renderer
-        var renderer = new CustomRender();
-        renderer.onParticleCreated = function(p) {
-            //p.target = mesh.clone();
-            p.target = this.targetPool.get(mesh);
-            p.target.position.copy(p.p);
-            scene.add(p.target);
-        }
-
-        renderer.onParticleUpdate = function(p) {
-            p.target.position.copy(p.p);
-            p.target.rotation.set(p.rotation.x, p.rotation.y, p.rotation.z);
-
-            var scale = p.scale * 30;
-            p.target.scale.set(scale, scale, scale);
-        }
-
-        renderer.onParticleDead = function(p) {
-            this.targetPool.expire(p.target);
-            scene.remove(p.target);
-            p.target = null;
-        }
-
-        proton.addRender(renderer);
-        return proton;
-    }
-
-    createEmitter(scene: THREE.Scene, proton: Proton) {
-        const emitter = new Emitter();
+        //  custom render particle demo
+        initCustomRenderParticle(scene, camera, renderer, control, mesh);
         
-        emitter.rate = new Rate(new Span(4, 8), new Span(.2, .5));
-        // @ts-ignore
-        emitter.addInitialize(new Mass(1));
-        // @ts-ignore
-        emitter.addInitialize(new Radius(100));
-        // @ts-ignore
-        emitter.addInitialize(new Life(2, 4));
-        // @ts-ignore
-        emitter.addInitialize(new Velocity(400, new Vector3D(0, 1, 0), 60));
-
-        emitter.addBehaviour(new Rotate("random", "random"));
-        emitter.addBehaviour(new Scale(1, .1));
-        emitter.addBehaviour(new Gravity(6));
-
-        var zone = new BoxZone(600);
-        zone.friction = 0.95;
-        zone.max = 7;
-        emitter.addBehaviour(new CrossZone(zone, "bound"));
-        emitter.addBehaviour(new Color(0xff0000, 'random', Infinity, ease.easeOutQuart));
-
-        // @ts-ignore
-        emitter.p.x = 0;
-        // @ts-ignore
-        emitter.p.y = 0;
-        // @ts-ignore
-        emitter.emit();
-        Debug.drawZone(proton, scene, zone);
-
-        return emitter;
     }
+
+
 }
